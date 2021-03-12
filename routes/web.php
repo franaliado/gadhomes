8673b068c728532a38f85225ce9a8a4b52d92e64
Fran Silva <franaliado@gmail.com>
2021-03-12 16:03:06 -0400

    Otro arreglo

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,9 @@ Route::resource('houses', 'Framing\HouseController')->middleware('auth');
 Route::get('/orders/{id}', 'Framing\OrderController@index')->middleware('auth');
 
 Route::get('/orders/{id}/create', 'Framing\OrderController@create')->middleware('auth');
-
+/*
 Route::get('/orders/{id}', 'Framing\OrderController@destroy')->middleware('auth');
+*/
 
 Route::resource('orders', 'Framing\OrderController')->middleware('auth');
 
