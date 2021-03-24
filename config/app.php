fe9731b43737907252221c34457a52b115df8db6
Joel Gomez <joelgomezb@gmail.com>
2021-03-23 23:01:17 -0400

    dompdf config file

--- config/app.php
+++ config/app.php
@@ -160,7 +160,8 @@ return [
         Illuminate\Session\SessionServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
-        Illuminate\View\ViewServiceProvider::class,
+	Illuminate\View\ViewServiceProvider::class,
+	Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -226,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+	'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
